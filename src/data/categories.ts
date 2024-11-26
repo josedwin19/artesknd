7698f1577f5f1c18340d69552d41143131c47852
josedwin19 <edwin19_7@hotmail.com>
2024-11-26 14:10:29 -0300

    update aretes link

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,7 +2,7 @@ export const categories = [
   {
     id: 'aretes',
     name: 'Aretes',
-    image: 'https://images.unsplash.com/photo-1535632787350-4e68ef0ac584?auto=format&fit=crop&q=80&w=800',
+    image: 'https://scontent-gru1-2.xx.fbcdn.net/v/t1.6435-9/121672988_155981929524905_6251094790121465330_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=91-Q4sEx3_QQ7kNvgEKBfpu&_nc_zt=23&_nc_ht=scontent-gru1-2.xx&_nc_gid=AzLCKGJGwKzK_FzhZbrQH03&oh=00_AYB-I4bCi6AT9nu1MdVRfmdvhw1nTddR-hwkjIQAIVI1lg&oe=676D8A34',
     description: 'Hermosos aretes artesanales hechos a mano con técnicas tradicionales peruanas.'
   },
   {
